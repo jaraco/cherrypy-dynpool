5d41b07aafc33792dfe9a473200dac41ee24ab5a
Jason R. Coombs <jaraco@jaraco.com>
2021-02-13 18:09:02 -0500

    Use declarative config to load readme.

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 
-import os
 import sys
 from setuptools import setup
 
-
-def root_dir():
-    rd = os.path.dirname(__file__)
-    if rd:
-        return rd
-    return '.'
 
 needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
 pytest_runner = ['pytest_runner'] if needs_pytest else []
@@ -24,7 +17,6 @@ setup_args = dict(
     license='Apache License 2.0',
     py_modules=['cherrypy_dynpool'],
     description='A dynamic thread pool tool for CherryPy.',
-    long_description=open(root_dir() + '/README').read(),
     install_requires=[
         "CherryPy>=3.2",
         "dynpool>=2.0,<3.0"
